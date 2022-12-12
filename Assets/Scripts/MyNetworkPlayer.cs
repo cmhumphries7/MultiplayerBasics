93ed3a3120c4b85acb4f81e6c84e2179b195898d
cmhumphries7 <cmh@chrishumphries.net>
2022-12-11 22:31:11 -0800

    Syncing Variables

---
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -0,0 +1,26 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNetworkPlayer : NetworkBehaviour
+
+{
+    [SyncVar]
+    [SerializeField]
+    private string displayName = "Missing Name";
+    [SyncVar]
+    [SerializeField]
+    private Color displayColor = Color.black;
+
+    [Server]
+    public void SetDisplayName (string newDisplayName)
+    {
+        displayName = newDisplayName;
+    }
+    [Server]
+    public void SetDisplayColor (Color newDisplayColor)
+    {
+        displayColor = newDisplayColor;
+    }
+}
